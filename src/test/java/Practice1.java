1b3cec868fc8793ac3cf2f6b14d9b19a48303b13
selmanland <selmanlanka@gmail.com>
2021-06-12 20:22:15 +0200

    EU5 work1

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -1,6 +1,7 @@
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("EU5 is liking Git on Intellij");
+        System.out.println("work1");
 
     }
 }
