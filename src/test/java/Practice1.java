3f3a85ef7588ba8f8ed04630844e03ee89c1135d
selmanland <selmanlanka@gmail.com>
2021-06-12 20:33:07 +0200

    doing more work

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -1,7 +1,12 @@
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("EU5 is liking Git on Intellij");
+
         System.out.println("work1");
 
+        //succesfully project to Github
+        //first Intellij automatically created remote repo
+        // created connection between local repo and remote repo
+        // pushed local commits to remote repo
     }
 }
